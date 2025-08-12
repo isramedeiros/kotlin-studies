c560d2f33a9985c0af24610e293994bf8d595866
Israel Medeiros <israelmfreitas12@gmail.com>
2025-08-11 21:58:26 -0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var nomeFaculdade = "FIAP"
+    println(nomeFaculdade)
+
+    nomeFaculdade = "Faculdade de Informática e Administração Paulista"
+    println(nomeFaculdade)
+
+    val nome = "Israel"
+    println(nome)
+}
