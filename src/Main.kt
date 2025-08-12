275b9eebda6fb2564d44dcfa8fc8c66ca3ba7769
Israel Medeiros <israelmfreitas12@gmail.com>
2025-08-12 10:18:45 -0300

    Studies in arrays and variables

--- src/Main.kt
+++ src/Main.kt
@@ -7,4 +7,28 @@ fun main(args: Array<String>) {
 
     val nome = "Israel"
     println(nome)
+
+    // calling a function
+    arrays()
+}
+
+fun arrays() {
+    // array vazio
+    val emptyArray = arrayOf<String>()
+
+    // array de strings
+    val shoppingList = arrayOf<String>("Leite", "Pão", "Manteiga", "Açúcar")
+
+    // usando inferência
+    val inferredShoppingList = arrayOf("Leite", "Pão", "Manteiga", "Açúcar")
+
+    // testando se um array está vazio
+    if (shoppingList.isEmpty()) {
+        println("A lista de compras está vazia")
+    } else {
+        println("A lista de compras não está vazia")
+    }
+
+    // recuperando o total de elementos do Array
+    println("Nossa lista de compras possui ${shoppingList.size} itens")
 }
