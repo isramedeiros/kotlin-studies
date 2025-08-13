a569384d8e08de4d63b841089dae09957629b3d1
Israel Medeiros <israelmfreitas12@gmail.com>
2025-08-13 08:20:33 -0300

    assigning and reassigning variables

---
+++ src/variables.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val nomeCachorro = "Theobaldo"
+    var sobrenomeCanino = "Canino"
+
+    println(nomeCachorro)
+    println(sobrenomeCanino)
+
+    sobrenomeCanino = "Lupus Familiaris"
+
+    println(nomeCachorro)
+    println(sobrenomeCanino)
+}
